b4f8fb2e885b0b6fb98565c069cf8673f16f6ffe
角田陸 <ukir19990530@gmail.com>
2026-09-09 23:50:49 +0900

    udpの勉強した

--- phase-03-python-socket/udp_client.py
+++ phase-03-python-socket/udp_client.py
@@ -7,6 +7,8 @@ PORT = 9000
 message = sys.argv[1] if len(sys.argv) > 1 else "hello"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# クライアント側はbindでポート番号を指定する必要はありません。OSが自動的に空いているポート番号を割り当てます。
 sock.sendto(message.encode(), (HOST, PORT))
 
 data, addr = sock.recvfrom(1024)
--- phase-03-python-socket/udp_server.py
+++ phase-03-python-socket/udp_server.py
@@ -3,11 +3,21 @@ import socket
 HOST = "127.0.0.1"
 PORT = 9000
 
+
+# socket.AF_INETはIPv4を使うことを意味します。AF=アドレスファミリー
+# socket.SOCK_DGRAMはUDPを使うことを意味します。
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# OSに対して、指定したIPアドレスとポート番号で待ち受けるように指示
 sock.bind((HOST, PORT))
 print(f"UDP server listening on {HOST}:{PORT}")
 
+# while Trueは無限ループを意味します。つまり、サーバーは永遠にクライアントからのメッセージを待ち受け続けます。
 while True:
+    # このデータの展開の仕方をアンパックという
+    # dataは受信したデータ、addrは送信元のアドレス情報(IPアドレスとポート番号)が入る
+    # 1024は受信するデータの最大バイト数を指定しています。1024バイトを超えるデータは切り捨てられます。
+    # recvfromはUDPの受信に使われるメソッド
+    # sendtoはUDPの送信に使われるメソッド
     data, addr = sock.recvfrom(1024)
     print(f"received {len(data)} bytes from {addr}: {data!r}")
     sock.sendto(data, addr)  # 受け取ったものをそのまま送り返す(echo)
